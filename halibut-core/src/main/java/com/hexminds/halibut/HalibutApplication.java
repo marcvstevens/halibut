87b390549c6201b97cf29a516ec14169395b3127
marcvstevens <marcvstevens@gmail.com>
2021-12-19 12:49:59 +0800

    new file:   halibut-core/pom.xml new file:   halibut-core/src/main/java/com/hexminds/halibut/HalibutApplication.java modified:   pom.xml deleted:    src/main/java/com/hexminds/halibut/App.java deleted:    src/test/java/com/hexminds/halibut/AppTest.java

---
+++ halibut-core/src/main/java/com/hexminds/halibut/HalibutApplication.java
@@ -0,0 +1,11 @@
+package com.hexminds.halibut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalibutApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HalibutApplication.class, args);
+    }
+}
